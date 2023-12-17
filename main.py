9d7e3d467326f69e0c4e312385cd745cc6f0211e
ARSHLAAN SIDDIQUI <128530488+ArshlaanSid@users.noreply.github.com>
2023-12-17 13:49:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('good')
+    print('change via github')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
